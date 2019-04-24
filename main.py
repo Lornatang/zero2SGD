e173a25fc5f68042f8556670a6ad6ed0b8110243
Lornatang <shiyipaisizuo@163.com>
2019-04-24 10:34:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from sklearn.linear_model import LinearRegression
-from ops import compute_grad, update_beta, rmse
+from ops import compute_grad_sgd, update_beta, loss
 
 import pandas as pd
 import numpy as np
@@ -10,31 +10,30 @@ test = pd.read_csv('data/test.csv')
 submit = pd.read_csv('data/sample_submit.csv')
 
 # initial setup params
-beta = [1, 1]
+beta = [0.5, 0.9]
 lr = 0.0001
-tol_L = 0.01
+tol_L = 0.1
 
 # normalizes x
 max_x = max(train['id'])
 x = train['id'] / max_x
 y = train['questions']
 
-
 # let's do the first calculation
 np.random.seed(10)
-grad = compute_grad(beta, x, y)
-loss = rmse(beta, x, y)
+grad = compute_grad_sgd(beta, x, y)
+losses = loss(beta, x, y)
 beta = update_beta(beta, lr, grad)
-loss_new = rmse(beta, x, y)
+new_losses = loss(beta, x, y)
 
 i = 1
-while np.abs(loss_new - loss) > tol_L:
+while np.abs(new_losses - losses) > tol_L:
   beta = update_beta(beta, lr, grad)
-  grad = compute_grad(beta, x, y)
+  grad = compute_grad_sgd(beta, x, y)
   if i % 1000 == 0:
-    loss = loss_new
-    loss_new = rmse(beta, x, y)
-    print(f'Round {i} Diff SGD {abs(loss_new - loss)}')
+    losses = new_losses
+    new_losses = loss(beta, x, y)
+    print(f'Iter {i} SGD loss {abs(new_losses - losses):.6f}')
   i += 1
 
 
@@ -47,7 +46,7 @@ print(f'Sklearn Coef: {val.coef_[0][0]}')
 print(f'Our     Intercept: {beta[0]}')
 print(f'Sklearn Intercept: {val.intercept_[0]}')
 
-our_rmse = rmse(beta, x, y)
-sklearn_rmse = rmse([val.intercept_[0], val.coef_[0][0]], train['id'], y)
-print(f'Our     RMSE: {our_rmse}')
-print(f"Sklearn RMSE: {sklearn_rmse}")
+our_losses = loss(beta, x, y)
+sklearn_losses = loss([val.intercept_[0], val.coef_[0][0]], train['id'], y)
+print(f'Our     RMSE: {our_losses}')
+print(f"Sklearn RMSE: {sklearn_losses}")
